20d13eadcf1bc1b21d190cb4185f7d4329a0f962
spartanon <ira@agoras.app>
2023-01-28 15:19:25 -0500

    add event on addCommit

--- contracts/BitLog.sol
+++ contracts/BitLog.sol
@@ -7,10 +7,13 @@ contract BitLog {
     mapping(uint256 => uint256) private _commitTime;
     mapping(address => uint256) private _commits;
 
+    event AddCommitEvent(address addr_);
+
     function addCommit(uint256 commitId_) public returns (bytes32) {
         bytes32 _hash = keccak256(abi.encodePacked(msg.sender, _commits[msg.sender]++));
         _commitId[_hash] = commitId_;
         _commitTime[commitId_] = block.timestamp;
+        emit AddCommitEvent(msg.sender);
         return _hash;
     }
 
